992ae35ec42335622cf1617653386c0e95f35556
Amisha Patidar <amishapatidar98@gmail.com>
2021-01-26 11:41:57 +0530

    Add take Input from user and display content on file using filestream classes

---
+++ ConFileInputOutput/ConFileInputOutput/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConFileInputOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StreamReader srr = null;
+            using (FileStream fstream = new FileStream("G:\\input.txt", FileMode.Create, FileAccess.ReadWrite))
+            {
+                using (StreamWriter srr = new StreamWriter(fstream))
+                { 
+                   Console.WriteLine("Write content to enter into File : ");
+                   srr.WriteLine(Console.ReadLine());
+                }
+                using (StreamReader sr = new StreamReader("G:\\input.txt"))
+                {
+                    //string line;
+                    //while((line = sr.ReadLine()) != null)
+                    //{
+                    //    Console.WriteLine(line);
+                    //}
+                   Console.WriteLine("Reading contents of input.txt : \n" + sr.ReadToEnd());
+                    
+                }
+            }
+        }
+    }
+}
